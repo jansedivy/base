42777830ed3d66e09d1ce3d4ae8e5aaa1d18ced9
Jan Sedivy <me@jansedivy.com>
2016-03-20 23:37:52 +0100

    fix bug with hash function

--- base/base.h
+++ base/base.h
@@ -3,7 +3,7 @@
 #include <stdint.h>
 
 namespace Base {
-  uint32_t hash_string(char *wstring, uint32_t hash) {
+  uint32_t hash_string(char *string, uint32_t hash=0) {
     while (*string != '\0') {
       char c = *string++;
       if ((c >= 'a') && (c<= 'z')) {
