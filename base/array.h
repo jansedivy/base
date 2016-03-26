ded90eef34c2d01a91d96a1cbf4566d0967526fb
Jan Sedivy <me@jansedivy.com>
2016-03-26 15:44:18 +0100

    add clang formatting

--- base/array.h
+++ base/array.h
@@ -4,8 +4,9 @@
 #include <stdlib.h>
 #include <memory>
 
-template<typename T> struct Array {
-  Array() : capacity(0), size(0), data(0) {}
+template <typename T> struct Array {
+  Array() : capacity(0), size(0), data(0) {
+  }
   ~Array() {
     free(data);
   }
@@ -20,15 +21,27 @@ template<typename T> struct Array {
 };
 
 namespace array {
-  template<typename T> inline T* begin(Array<T> &array) { return array.data; }
-  template<typename T> inline const T* begin(const Array<T> &array) { return array.data; }
-  template<typename T> inline T* end(Array<T> &array) { return array.data + array.size; }
-  template<typename T> inline const T* end(const Array<T> &array) { return array.data + array.size; }
+  template <typename T> inline T *begin(Array<T> &array) {
+    return array.data;
+  }
+  template <typename T> inline const T *begin(const Array<T> &array) {
+    return array.data;
+  }
+  template <typename T> inline T *end(Array<T> &array) {
+    return array.data + array.size;
+  }
+  template <typename T> inline const T *end(const Array<T> &array) {
+    return array.data + array.size;
+  }
 
-  template<typename T> inline bool empty(const Array<T> &array) { return array.size == 0; }
-  template<typename T> inline bool any(const Array<T> &array) { return array.size != 0; }
+  template <typename T> inline bool empty(const Array<T> &array) {
+    return array.size == 0;
+  }
+  template <typename T> inline bool any(const Array<T> &array) {
+    return array.size != 0;
+  }
 
-  template<typename T> void set_capacity(Array<T> &array, uint32_t new_capacity) {
+  template <typename T> void set_capacity(Array<T> &array, uint32_t new_capacity) {
     if (new_capacity == array.capacity) {
       return;
     }
@@ -47,17 +60,17 @@ namespace array {
     array.capacity = new_capacity;
   }
 
-  template<typename T> inline void trim(Array<T> &array) {
+  template <typename T> inline void trim(Array<T> &array) {
     set_capacity(array, array.size);
   }
 
-  template<typename T> inline void reserve(Array<T> &array, uint32_t new_capacity) {
+  template <typename T> inline void reserve(Array<T> &array, uint32_t new_capacity) {
     if (new_capacity > array.capacity) {
       set_capacity(array, new_capacity);
     }
   }
 
-  template<typename T> void grow(Array<T> &array, uint32_t min_capacity) {
+  template <typename T> void grow(Array<T> &array, uint32_t min_capacity) {
     uint32_t new_capacity = array.capacity * 2 + 8;
     if (new_capacity < min_capacity) {
       new_capacity = min_capacity;
@@ -73,7 +86,7 @@ namespace array {
     array.size = new_size;
   }
 
-  template<typename T> inline void push_back(Array<T> &array, T value) {
+  template <typename T> inline void push_back(Array<T> &array, T value) {
     if (array.size + 1 > array.capacity) {
       grow(array, array.size + 1);
     }
@@ -81,24 +94,24 @@ namespace array {
     array.data[array.size++] = value;
   }
 
-  template<typename T> inline void pop_back(Array<T> &array) {
+  template <typename T> inline void pop_back(Array<T> &array) {
     array.size -= 1;
   }
 
-  template<typename T> inline void clear(Array<T> &array) {
+  template <typename T> inline void clear(Array<T> &array) {
     array.size = 0;
   }
 };
 
-template<typename T> inline T &Array<T>::operator[](uint32_t index) {
+template <typename T> inline T &Array<T>::operator[](uint32_t index) {
   return data[index];
 }
 
-template<typename T> const T &Array<T>::operator[](uint32_t index) const {
+template <typename T> const T &Array<T>::operator[](uint32_t index) const {
   return data[index];
 }
 
-template<typename T> Array<T> &Array<T>::operator=(const Array<T> &other) {
+template <typename T> Array<T> &Array<T>::operator=(const Array<T> &other) {
   array::set_capacity(*this, other.size);
   size = other.size;
   memcpy(data, other.data, sizeof(T) * other.size);
--- base/hash.h
+++ base/hash.h
@@ -2,8 +2,7 @@
 
 #include "array.h"
 
-template<typename T> struct Hash
-{
+template <typename T> struct Hash {
   struct Entry {
     uint64_t key;
     uint32_t next;
@@ -14,72 +13,67 @@ template<typename T> struct Hash
   Array<Entry> entries;
 };
 
-namespace hash
-{
+namespace hash {
   /// Returns true if the specified key exists in the hash.
-  template<typename T> bool has(const Hash<T> &h, uint64_t key);
+  template <typename T> bool has(const Hash<T> &h, uint64_t key);
 
   /// Returns the value stored for the specified key, or deffault if the key
   /// does not exist in the hash.
-  template<typename T> const T &get(const Hash<T> &h, uint64_t key, const T &deffault);
+  template <typename T> const T &get(const Hash<T> &h, uint64_t key, const T &deffault);
 
   /// Sets the value for the key.
-  template<typename T> void set(Hash<T> &h, uint64_t key, const T &value);
+  template <typename T> void set(Hash<T> &h, uint64_t key, const T &value);
 
   /// Removes the key from the hash if it exists.
-  template<typename T> void remove(Hash<T> &h, uint64_t key);
+  template <typename T> void remove(Hash<T> &h, uint64_t key);
 
   /// Resizes the hash lookup table to the specified size.
   /// (The table will grow automatically when 70 % full.)
-  template<typename T> void reserve(Hash<T> &h, uint32_t size);
+  template <typename T> void reserve(Hash<T> &h, uint32_t size);
 
   /// Remove all elements from the hash.
-  template<typename T> void clear(Hash<T> &h);
+  template <typename T> void clear(Hash<T> &h);
 
   /// Returns a pointer to the first entry in the hash table, can be used to
   /// efficiently iterate over the elements (in random order).
-  template<typename T> const typename Hash<T>::Entry *begin(const Hash<T> &h);
-  template<typename T> const typename Hash<T>::Entry *end(const Hash<T> &h);
+  template <typename T> const typename Hash<T>::Entry *begin(const Hash<T> &h);
+  template <typename T> const typename Hash<T>::Entry *end(const Hash<T> &h);
 }
 
-namespace multi_hash
-{
+namespace multi_hash {
   /// Finds the first entry with the specified key.
-  template<typename T> const typename Hash<T>::Entry *find_first(const Hash<T> &h, uint64_t key);
+  template <typename T> const typename Hash<T>::Entry *find_first(const Hash<T> &h, uint64_t key);
 
   /// Finds the next entry with the same key as e.
-  template<typename T> const typename Hash<T>::Entry *find_next(const Hash<T> &h, const typename Hash<T>::Entry *e);
+  template <typename T> const typename Hash<T>::Entry *find_next(const Hash<T> &h, const typename Hash<T>::Entry *e);
 
   /// Returns the number of entries with the key.
-  template<typename T> uint32_t count(const Hash<T> &h, uint64_t key);
+  template <typename T> uint32_t count(const Hash<T> &h, uint64_t key);
 
   /// Returns all the entries with the specified key.
   /// Use a TempAllocator for the array to avoid allocating memory.
-  template<typename T> void get(const Hash<T> &h, uint64_t key, Array<T> &items);
+  template <typename T> void get(const Hash<T> &h, uint64_t key, Array<T> &items);
 
   /// Inserts the value as an aditional value for the key.
-  template<typename T> void insert(Hash<T> &h, uint64_t key, const T &value);
+  template <typename T> void insert(Hash<T> &h, uint64_t key, const T &value);
 
   /// Removes the specified entry.
-  template<typename T> void remove(Hash<T> &h, const typename Hash<T>::Entry *e);
+  template <typename T> void remove(Hash<T> &h, const typename Hash<T>::Entry *e);
 
   /// Removes all entries with the specified key.
-  template<typename T> void remove_all(Hash<T> &h, uint64_t key);
+  template <typename T> void remove_all(Hash<T> &h, uint64_t key);
 }
 
-namespace hash_internal
-{
+namespace hash_internal {
   const uint32_t END_OF_LIST = 0xffffffffu;
 
-  struct FindResult
-  {
+  struct FindResult {
     uint32_t hash_i;
     uint32_t data_prev;
     uint32_t data_i;
   };
 
-  template<typename T> uint32_t add_entry(Hash<T> &h, uint64_t key)
-  {
+  template <typename T> uint32_t add_entry(Hash<T> &h, uint64_t key) {
     typename Hash<T>::Entry e;
     e.key = key;
     e.next = END_OF_LIST;
@@ -88,8 +82,7 @@ namespace hash_internal
     return ei;
   }
 
-  template<typename T> void erase(Hash<T> &h, const FindResult &fr)
-  {
+  template <typename T> void erase(Hash<T> &h, const FindResult &fr) {
     if (fr.data_prev == END_OF_LIST)
       h.hash[fr.hash_i] = h.entries[fr.data_i].next;
     else
@@ -109,8 +102,7 @@ namespace hash_internal
       h.hash[last.hash_i] = fr.data_i;
   }
 
-  template<typename T> FindResult find(const Hash<T> &h, uint64_t key)
-  {
+  template <typename T> FindResult find(const Hash<T> &h, uint64_t key) {
     FindResult fr;
     fr.hash_i = END_OF_LIST;
     fr.data_prev = END_OF_LIST;
@@ -130,8 +122,7 @@ namespace hash_internal
     return fr;
   }
 
-  template<typename T> FindResult find(const Hash<T> &h, const typename Hash<T>::Entry *e)
-  {
+  template <typename T> FindResult find(const Hash<T> &h, const typename Hash<T>::Entry *e) {
     FindResult fr;
     fr.hash_i = END_OF_LIST;
     fr.data_prev = END_OF_LIST;
@@ -151,13 +142,11 @@ namespace hash_internal
     return fr;
   }
 
-  template<typename T> uint32_t find_or_fail(const Hash<T> &h, uint64_t key)
-  {
+  template <typename T> uint32_t find_or_fail(const Hash<T> &h, uint64_t key) {
     return find(h, key).data_i;
   }
 
-  template<typename T> uint32_t find_or_make(Hash<T> &h, uint64_t key)
-  {
+  template <typename T> uint32_t find_or_make(Hash<T> &h, uint64_t key) {
     const FindResult fr = find(h, key);
     if (fr.data_i != END_OF_LIST)
       return fr.data_i;
@@ -170,8 +159,7 @@ namespace hash_internal
     return i;
   }
 
-  template<typename T> uint32_t make(Hash<T> &h, uint64_t key)
-  {
+  template <typename T> uint32_t make(Hash<T> &h, uint64_t key) {
     const FindResult fr = find(h, key);
     const uint32_t i = add_entry(h, key);
 
@@ -184,21 +172,19 @@ namespace hash_internal
     return i;
   }
 
-  template<typename T> void find_and_erase(Hash<T> &h, uint64_t key)
-  {
+  template <typename T> void find_and_erase(Hash<T> &h, uint64_t key) {
     const FindResult fr = find(h, key);
     if (fr.data_i != END_OF_LIST)
       erase(h, fr);
   }
 
-  template<typename T> void rehash(Hash<T> &h, uint32_t new_size)
-  {
+  template <typename T> void rehash(Hash<T> &h, uint32_t new_size) {
     Hash<T> nh = {};
     array::resize(nh.hash, new_size);
     array::reserve(nh.entries, h.entries.size);
-    for (uint32_t i=0; i<new_size; ++i)
+    for (uint32_t i = 0; i < new_size; ++i)
       nh.hash[i] = END_OF_LIST;
-    for (uint32_t i=0; i<h.entries.size; ++i) {
+    for (uint32_t i = 0; i < h.entries.size; ++i) {
       const typename Hash<T>::Entry &e = h.entries[i];
       multi_hash::insert(nh, e.key, e.value);
     }
@@ -209,34 +195,28 @@ namespace hash_internal
     memcpy(&nh, &empty, sizeof(Hash<T>));
   }
 
-  template<typename T> bool full(const Hash<T> &h)
-  {
+  template <typename T> bool full(const Hash<T> &h) {
     const float max_load_factor = 0.7f;
     return h.entries.size >= h.hash.size * max_load_factor;
   }
 
-  template<typename T> void grow(Hash<T> &h)
-  {
+  template <typename T> void grow(Hash<T> &h) {
     const uint32_t new_size = h.entries.size * 2 + 10;
     rehash(h, new_size);
   }
 }
 
-namespace hash
-{
-  template<typename T> bool has(const Hash<T> &h, uint64_t key)
-  {
+namespace hash {
+  template <typename T> bool has(const Hash<T> &h, uint64_t key) {
     return hash_internal::find_or_fail(h, key) != hash_internal::END_OF_LIST;
   }
 
-  template<typename T> const T &get(const Hash<T> &h, uint64_t key, const T &deffault)
-  {
+  template <typename T> const T &get(const Hash<T> &h, uint64_t key, const T &deffault) {
     const uint32_t i = hash_internal::find_or_fail(h, key);
     return i == hash_internal::END_OF_LIST ? deffault : h.entries[i].value;
   }
 
-  template<typename T> void set(Hash<T> &h, uint64_t key, const T &value)
-  {
+  template <typename T> void set(Hash<T> &h, uint64_t key, const T &value) {
     if (h.hash.size == 0)
       hash_internal::grow(h);
 
@@ -246,43 +226,35 @@ namespace hash
       hash_internal::grow(h);
   }
 
-  template<typename T> void remove(Hash<T> &h, uint64_t key)
-  {
+  template <typename T> void remove(Hash<T> &h, uint64_t key) {
     hash_internal::find_and_erase(h, key);
   }
 
-  template<typename T> void reserve(Hash<T> &h, uint32_t size)
-  {
+  template <typename T> void reserve(Hash<T> &h, uint32_t size) {
     hash_internal::rehash(h, size);
   }
 
-  template<typename T> void clear(Hash<T> &h)
-  {
+  template <typename T> void clear(Hash<T> &h) {
     array::clear(h.entries);
     array::clear(h.hash);
   }
 
-  template<typename T> const typename Hash<T>::Entry *begin(const Hash<T> &h)
-  {
+  template <typename T> const typename Hash<T>::Entry *begin(const Hash<T> &h) {
     return array::begin(h.entries);
   }
 
-  template<typename T> const typename Hash<T>::Entry *end(const Hash<T> &h)
-  {
+  template <typename T> const typename Hash<T>::Entry *end(const Hash<T> &h) {
     return array::end(h.entries);
   }
 }
 
-namespace multi_hash
-{
-  template<typename T> const typename Hash<T>::Entry *find_first(const Hash<T> &h, uint64_t key)
-  {
+namespace multi_hash {
+  template <typename T> const typename Hash<T>::Entry *find_first(const Hash<T> &h, uint64_t key) {
     const uint32_t i = hash_internal::find_or_fail(h, key);
     return i == hash_internal::END_OF_LIST ? 0 : &h.entries[i];
   }
 
-  template<typename T> const typename Hash<T>::Entry *find_next(const Hash<T> &h, const typename Hash<T>::Entry *e)
-  {
+  template <typename T> const typename Hash<T>::Entry *find_next(const Hash<T> &h, const typename Hash<T>::Entry *e) {
     uint32_t i = e->next;
     while (i != hash_internal::END_OF_LIST) {
       if (h.entries[i].key == e->key)
@@ -292,8 +264,7 @@ namespace multi_hash
     return 0;
   }
 
-  template<typename T> uint32_t count(const Hash<T> &h, uint64_t key)
-  {
+  template <typename T> uint32_t count(const Hash<T> &h, uint64_t key) {
     uint32_t i = 0;
     const typename Hash<T>::Entry *e = find_first(h, key);
     while (e) {
@@ -303,8 +274,7 @@ namespace multi_hash
     return i;
   }
 
-  template<typename T> void get(const Hash<T> &h, uint64_t key, Array<T> &items)
-  {
+  template <typename T> void get(const Hash<T> &h, uint64_t key, Array<T> &items) {
     const typename Hash<T>::Entry *e = find_first(h, key);
     while (e) {
       array::push_back(items, e->value);
@@ -312,8 +282,7 @@ namespace multi_hash
     }
   }
 
-  template<typename T> void insert(Hash<T> &h, uint64_t key, const T &value)
-  {
+  template <typename T> void insert(Hash<T> &h, uint64_t key, const T &value) {
     if (h.hash.size == 0)
       hash_internal::grow(h);
 
@@ -323,15 +292,13 @@ namespace multi_hash
       hash_internal::grow(h);
   }
 
-  template<typename T> void remove(Hash<T> &h, const typename Hash<T>::Entry *e)
-  {
+  template <typename T> void remove(Hash<T> &h, const typename Hash<T>::Entry *e) {
     const hash_internal::FindResult fr = hash_internal::find(h, e);
     if (fr.data_i != hash_internal::END_OF_LIST)
       hash_internal::erase(h, fr);
   }
 
-  template<typename T> void remove_all(Hash<T> &h, uint64_t key)
-  {
+  template <typename T> void remove_all(Hash<T> &h, uint64_t key) {
     while (hash::has(h, key))
       hash::remove(h, key);
   }
